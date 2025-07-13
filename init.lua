e3dec535d5061467ed238bb1b506f2dcb9181600
Max Zhenzhera <maxzhenzhera@gmail.com>
2025-07-13 12:43:41 +0300

    set jj for esc

--- init.lua
+++ init.lua
@@ -1018,3 +1018,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
